076de28f430d02a871af6d0c1fcaf94db55fdbda
Prakhar_Github <prakhar.5779.aps3@gmail.com>
2024-09-09 19:42:47 +0530

    Ai functionality added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,20 @@
 // import React from 'react'
+import { useContext } from "react";
 import { assets } from "../../assets/assets.js";
+import { Context } from "../../context/Context.jsx";
 import "./Main.css";
 
 const Main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+  } = useContext(Context);
+
   return (
     <div className="main">
       <div className="nav">
@@ -11,60 +23,92 @@ const Main = () => {
       </div>
 
       <div className="main-container">
-        <div className="greet">
-          <p>
-            <span>Hello, Dev</span>
-          </p>
-          <p>How can I help you Today?</p>
-        </div>
+        {/* SHOWING THE RESULT WHEN AVAILABLE */}
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Dev</span>
+              </p>
+              <p>How can I help you Today?</p>
+            </div>
 
-        {/* CARDS */}
-        <div className="cards">
-          <div className="card">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto,
-              consequatur.
-            </p>
-            <img src={assets.compass_icon} alt="icon" />
-          </div>
+            {/* CARDS */}
+            <div className="cards">
+              <div className="card">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Iusto, consequatur.
+                </p>
+                <img src={assets.compass_icon} alt="icon" />
+              </div>
 
-          <div className="card">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto,
-              consequatur.
-            </p>
-            <img src={assets.bulb_icon} alt="icon" />
-          </div>
+              <div className="card">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Iusto, consequatur.
+                </p>
+                <img src={assets.bulb_icon} alt="icon" />
+              </div>
 
-          <div className="card">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto,
-              consequatur.
-            </p>
-            <img src={assets.message_icon} alt="icon" />
-          </div>
+              <div className="card">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Iusto, consequatur.
+                </p>
+                <img src={assets.message_icon} alt="icon" />
+              </div>
 
-          <div className="card">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto,
-              consequatur.
-            </p>
-            <img src={assets.code_icon} alt="icon" />
+              <div className="card">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Iusto, consequatur.
+                </p>
+                <img src={assets.code_icon} alt="icon" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="icon" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+
+              {/* <p>{resultData}</p> */}
+            </div>
           </div>
-        </div>
+        )}
 
         {/* BOTTOM SECTION */}
         <div className="main-bottom">
           <div className="search-box">
-            <input type="text" placeholder="Enter a prompt here" />
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter a prompt here"
+            />
             <div>
               <img src={assets.gallery_icon} alt="icon" />
               <img src={assets.mic_icon} alt="icon" />
-              <img src={assets.send_icon} alt="icon" />
+              <img onClick={() => onSent()} src={assets.send_icon} alt="icon" />
             </div>
           </div>
           <p className="bottom-info">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ea ratione ipsum distinctio, enim error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ea
+            ratione ipsum distinctio, enim error.
           </p>
         </div>
       </div>
---
+++ src/context/Context.jsx
@@ -0,0 +1,45 @@
+import { createContext } from "react";
+import run from "../config/gemini.js";
+import { useState } from "react";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompt, setPrevPrompt] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const onSent = async (prompt) => {
+    setResultData(""); // remove prev response
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input)
+    const response = await run(input);
+    setResultData(response);
+    setLoading(false);
+    setInput("");
+  };
+
+  // onSent("what is Express js")
+  const contextValue = {
+    prevPrompt,
+    setPrevPrompt,
+    onSent,
+    recentPrompt,
+    setRecentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+  };
+
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
